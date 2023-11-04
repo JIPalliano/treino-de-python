c7e26af4680b56d16d1628c4741f5ea08f8d71f2
ismael.palliano <ismael.palliano@ufcspa.edu.br>
2023-11-04 11:05:14 -0300

    lambda de raiz quadrada e potenciação

--- parquinho.py
+++ parquinho.py
@@ -1,7 +1,21 @@
+import math as mt
+
 #treinamento de lambda
 
 #to_alternating_case = lambda string: string.swapcase()
 
-merge_arrays=lambda a,b:sorted({*(a+b)})
+#merge_arrays=lambda a,b:sorted({*(a+b)})
 
-lista = merge_arrays([1,2,3], [0,2,4])
+#lista = merge_arrays([1,2,3], [0,2,4])
+
+
+#test = lambda x: [ int(x[i] **(1/2)) for i in range(len(x))]
+
+square_or_square_root=lambda arr: [e**(1/2) if e**(1/2)%1==0 else e**2 for e in arr ]
+
+# [2, 9, 3, 49, 4, 1]
+lista = [4, 3, 9, 7, 2, 1]
+
+print(square_or_square_root(lista))
+
+#print(int(4**(1/2)))
