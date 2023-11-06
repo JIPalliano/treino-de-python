e620aac2e3d6b51ff9b383eb025c40b6a71c8c85
ismael.palliano <ismael.palliano@ufcspa.edu.br>
2023-11-06 10:53:38 -0300

    treinamento de lambda codewars

--- parquinho.py
+++ parquinho.py
@@ -1,10 +1,17 @@
-def distance_between_points(a, b):
-    Ax = a[0] #1
-    Ay = a[1] #6
-    Bx = b[0] #4
-    By = b[1] #2
-    x = ((Ax-Bx)**2)+((Ay-By)**2)
-    r = x **(1/2)
-    return r
+def get_count(sentence):
+    list = []
+    n = 0
+    for i in sentence:
+        if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+            list.append(i)
+            n +=1
+    return n
 
-print(distance_between_points((-10.2,12.5), (0.3,14.7)))
+def getCount(inputStr):
+    return sum(1 for let in inputStr if let in "aeiouAEIOU")
+
+getCountLambda = lambda inputStr: sum(1 for let in inputStr if let in "aeiouAEIOU")
+
+print(getCount("aeioux"))
+print(get_count("aeioux"))
+print(getCountLambda("aeioux"))
