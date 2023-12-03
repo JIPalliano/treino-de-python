21165020ecd607c132f1087dea5e26520ff793b3
ismael.palliano <ismael.palliano@ufcspa.edu.br>
2023-12-03 15:38:53 -0300

    final da atividade extensionista

--- atividade_extensionista.py
+++ atividade_extensionista.py
@@ -1,19 +1,25 @@
+#Importação da ferramenta para reconhecer a voz
 import speech_recognition as sr
+#Importação da ferramenta para fazer o processo de automação no windows
 import pyautogui as at
-import time as sleep
 
+
+
+#Função que irá capturar o áudio do usuário
 def ouvir_microfone():
+    #Instanciação do método Recognizer, para reconhecer o áudio
     microfone = sr.Recognizer()
     
     with sr.Microphone() as source:
         microfone.adjust_for_ambient_noise(source)
-        print("Fale algo:")
+        print("Escolha uma das opção ou fale algo:")
         audio = microfone.listen(source)
         
     try:
-        
+        #Configurando para que o programa entenda a língua portuguesa
         frase = microfone.recognize_google(audio, language='pt-BR')
-            
+        
+        #Mostra o foi dito pelo usuário
         print("Você falou: "+frase)
         
     except sr.UnknownValueError:
@@ -21,9 +27,13 @@ def ouvir_microfone():
         
     return frase
 
+#Função que irá executar o Google
 def exec_navegador():
+    #Inicia o menu iniciar
     at.press('win')
+    #Pesquisa o Google no menu iniciar
     at.write('google')
+    #Executa o navegador
     at.press('Enter')
     print("Deseja pesquisar sobre o que?")
     frase = ouvir_microfone()
@@ -32,21 +42,25 @@ def exec_navegador():
     
     repete = True
     
+    #Aqui no laço de repetição vai servir para que o usuário navegue pelo browser
     while repete:
         print("Quer pesquisar sobre oq na página?")
         frase = ouvir_microfone()
+        #Rola o "scroll" baixo
         if "baixo" in frase:
             at.scroll(-300)
+        #Rola o "scroll" para cima
         if "cima" in frase:
             at.scroll(300)
+        #Pesquisa a opção que o usuário quer com as alternativas que o google sugerir
         if "pesquisar" in frase:
             novamente = True
             at.hotkey('ctrl','f')
-            print("Oq procura?")
+            print("Esta procurando sobre o que?")
             frase = ouvir_microfone()
             at.write(frase)
             while novamente:
-                print("Achou oq procura?")
+                print("Encontrou o que procura?")
                 frase = ouvir_microfone()
                 if "sim" in frase:
                     at.hotkey('ctrl','Enter')
@@ -55,38 +69,58 @@ def exec_navegador():
                     at.hotkey('ctrl','g')
                 if "sair" in frase:
                     novamente = False
-            
+        #Sair do Programa
         if "sair" in frase:
             repete = False
 
     
+#Função que irá executar o Bloco de notas
+def exec_bloco_de_notas():
+    at.press('win')
+    at.write('Bloco de notas')
+    at.press('Enter')
     
-def exec_word():
-    return print("entrou exec_word")
-
-
-
-
-
-repete = True
-while repete:
+    print("Deseja escrever algo?")
     frase = ouvir_microfone()
+    at.write(frase)
+    
+    repete = True
+    #Laço de repetição irá digitar tudo que o usuário dizer
+    while repete:
+        print("Quer escrever algo a mais?")
+        resposta = ouvir_microfone()
+        if "sim" in resposta:
+            print("Fale a frase:")
+            frase = ouvir_microfone()
+            at.write(frase)
+        if "não" in resposta:
+            repete = False
+
+
+
+
+
+#inicialização do programa usabilidade do Windows via comando de voz
+repete = True
+#estrutura de repetição para o usuário escolher sua opção
+while repete:
+    #prints da escolha do usuário
+    print("Escolha uma das opções de navegação:\n 1 - Iniciar o Google \n 2 - Iniciar o bloco de notas \n 3 - Encerrar programa")
+    #Função que vai receber o áudio do usuário
+    frase = ouvir_microfone()
+    #move o mouse no centro para facilitar o uso do "scroll" na página
     at.moveTo(960,540)
+    
+    
     #Google
-    if "navegador" in frase:
+    if "navegador" in frase or "1" in frase or "um" in frase:
+        #ira executar o google
         exec_navegador()
     #Word
-    if "Word" in frase:
-        exec_word()
-    if "sair" in frase:
+    if "escrever" in frase or "2" in frase or "dois" in frase:
+        #ira executar o bloco de notas
+        exec_bloco_de_notas()
+    if "sair" in frase or "3" in frase or "três" in frase:
+        #encerra o programa
         print("Encerrando programa")
         repete = False
-
-#repete = True
-
-#while repete:
-    #print("Deseja mais alguma coisa?")
-    #frase = ouvir_microfone()
-    
-    #if "encerrar execução Python" in frase:
-        #repete = False
